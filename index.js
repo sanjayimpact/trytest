d1145392aeb45948baf582c7f011e18b8ba86d42
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2025-06-23 10:53:07 +0530

    main

---
+++ index.js
@@ -0,0 +1,30 @@
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+
+puppeteer.use(StealthPlugin());
+
+const run = async () => {
+  console.log('🚀 Launching Chromium...');
+  const browser = await puppeteer.launch({
+    headless: false,
+    // executablePath: '/usr/bin/chromium-browser', // Ubuntu
+    // or '/usr/bin/chromium' on Amazon Linux
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
+
+  const page = await browser.newPage();
+  console.log('🌐 Navigating to example.com...');
+  await page.goto('https://example.com');
+const textContent = await page.evaluate(() => {
+    return document.body.innerText;
+  });
+
+  console.log('📄 Page Text:\n', textContent); // <- Logs fu
+  console.log('📸 Taking screenshot...');
+  await page.screenshot({ path: 'ec2-test.png' });
+
+await browser.close();
+  console.log('✅ Done! Screenshot saved as ec2-test.png');
+};
+
+run();
